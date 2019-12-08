f2823810e078c8aae3c602559e41ca0e62c5b7e3
Your Name <you@example.com>
2019-12-08 21:54:33 +0100

    Add file

--- src/main.c
+++ src/main.c
@@ -12,38 +12,42 @@ static int len = 10; //Resolution of ADC or expected No. of bits.
 void sigHandler(void);
 
 int main (void){
+	
+	
 	//Data dumpfile
 	FILE *dataDump;
-	printf("Create new Dump-file..");
+	printf("Create new Dump-file..\n");
 	dataDump = fopen("./dataDump.txt", "a+"); //Creation, open for r/w at file end.
 	
 	if(dataDump == NULL){
-		printf("Unable to create Dump-file");
-		exit(EXIT_FAILURE);
+		printf("Unable to create Dump-file\n");
+		exit(1);
 	}else
-		printf("Dump-file created.");
+		printf("Done.\n");
 		
 	
-	//init. spi      	
-	printf("Setup the SPI interface..");
-	int spiErr = wiringPiSPISetup(channel, speed); //Setup the SPI-Bus on CE0 and CLK
-	if(spiErr == -1){
-		printf("Failed to initialize the SPI-Bus.");
-		exit(EXIT_FAILURE);
+	//Setup the SPI-Bus on CE0 and init. CLK.     	
+	printf("Setup the SPI interface..\n");
+	if(wiringPiSPISetup() == -1){
+		printf("Failed to initialize the SPI-Bus.\n");
+		exit(1);
 	}
 	else{
-		printf("Done.");
-		delay(1000);
+		printf("Done.\n");
+		pinMode(10, OUTPUT); //Set CE0 as output
+		pinMode(12, OUTPUT); //Set MOSI as output	
+		pinMode(13, INPUT); //Set MISO as input
+		delay(5000);
 	}
 	/*Initialize the chip like given in the datasheet #CS/SHDN -> LOW; If started with CE0 on LOW u need to toggle first.
 	 *By default on my PI3/4 the CS0 was on High. It's needed to invert it to initiate communication (MCP3008 Datasheet).
 	 * -> gpio readall
 	 **/
-	printf("Initialize MCP3008 communication..");
+	printf("Initialize MCP3008 communication..\n");
 	digitalWrite(10, LOW);
 	
-	printf("Done.");
-	delay(1000);
+	printf("Done.\n");
+	delay(5000);
 
 	//Read stuff
 	unsigned char *data;
