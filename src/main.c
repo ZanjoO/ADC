b72e16293e9da2942242d6653f432d15da21bc1f
Your Name <you@example.com>
2019-12-09 02:05:21 +0100

    Update

--- src/main.c
+++ src/main.c
@@ -44,7 +44,7 @@ int main (void){
 	//Make sure that toggle from 1 to 0
 	digitalWrite(CS, 1);
 	delay(5000);
-	printf("CS 1")
+	printf("CS 1");
 	unsigned char data[3] = {0x01, 0x80, 0x00};
 	printf("Test");
 	while(1){
