424469d93ecfac8aca6136e5324fe15700109210
Sam Nürenberg <sam.nuerenberg@web.de>
2020-01-18 21:34:28 +0100

    Update

--- SenderSrc/SenderMain.c
+++ SenderSrc/SenderMain.c
@@ -140,8 +140,8 @@ int main (void){
 			wiringPiSPIDataRW( CS, data, LEN );
 			res[i] = doDecimal(data);
 		}
-		convertHostShortToNetShort(res, toSend);
-		send_Data(&sock, toSend, sizeof(toSend), addr, PORT);
+		//convertHostShortToNetShort(res, toSend);
+		send_Data(&sock, res, sizeof(res), addr, PORT);
 	}	
 }
 
