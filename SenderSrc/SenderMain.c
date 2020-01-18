33f7e912e6737d682b14f1a293d7d9720bfdf339
Sam Nürenberg <sam.nuerenberg@web.de>
2020-01-18 07:16:20 +0100

    fix

--- SenderSrc/SenderMain.c
+++ SenderSrc/SenderMain.c
@@ -101,7 +101,7 @@ int main (void){
 
 		wiringPiSPIDataRW( CS, data, LEN );
 		res = htons(res); //Convert from host to network byte order
-		send_Data(&sock, res, sizeof(res), addr, PORT);
+		send_Data(&sock, &res, sizeof(res), addr, PORT);
 	}	
 }
 /*
